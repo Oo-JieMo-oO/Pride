ae2fe1d7badb328a3fa10035175914b0ba4fb777
Cc <@None>
2023-01-14 23:47:58 +0800

    爱尔兰诗人罗伊·克里夫特曾写下这样的诗句：
    我爱你，不光因为你的样子，
    
    还因为和你在一起时，我的样子。
    
    我爱你，不光因为你为我而做的事，
    
    还因为为了你，我能做的事。
    
    你理想中最美好的爱情是什么样的？
    
    是时时刻刻在一起的甜蜜；还是既享受一个人的独立，又拥有两个人的浪漫
    
    是执子之手与子偕老的从一而终；还是活在当下，爱得及时，爱得疯狂
    
    是矛盾，理解，包容
    
    是一见倾心，日久生情
    
    还是越过山海，兜兜转转还是你

--- shared/main/java/me/sound/Sound.java
+++ shared/main/java/me/sound/Sound.java
@@ -13,51 +13,8 @@ import java.awt.*;
 public class Sound {
     public static Sound INSTANCE;
     public static final Minecraft mc = Minecraft.getMinecraft();
-    public static boolean canSendMotionPacket = true;
+
     private static boolean notificationsAllowed = false;
-
-    public static boolean hasOptifine = false;
-    public static EntityRenderer getEntityRenderer() {
-        return getMinecraft().entityRenderer;
-    }
-
-    public static Minecraft getMinecraft() {
-        return Minecraft.getMinecraft();
-    }
-
-
-
-    public static PlayerControllerMP getPlayerController() {
-        return getMinecraft().playerController;
-    }
-
-
-
-    public static GameSettings getGameSettings() {
-        return getMinecraft().gameSettings;
-    }
-
-//    public static Timer getTimer() {
-//        return ((IMinecraft) Minecraft.getMinecraft()).getTimer();
-//    }
-
-
-
-
-    public static boolean notificationsAllowed() {
-        return notificationsAllowed;
-    }
-
-
-
-
-    public static Color getClientColor() {
-        return new Color(236, 133, 209);
-    }
-
-    public static Color getAlternateClientColor() {
-        return new Color(28, 167, 222);
-    }
 
     public static void notificationsAllowed(boolean value) {
         notificationsAllowed = value;
@@ -71,11 +28,5 @@ public class Sound {
 
         new SoundPlayer().playSound(SoundPlayer.SoundType.VICTORY, LiquidBounce.moduleManager.getToggleVolume());
     }
-    public boolean fastRenderDisabled(GameSettings gameSettingsIn) {
-        try {
-            return !((boolean) this.fastRenderDisabled(gameSettingsIn));
-        } catch (Exception ignored) {
-        }
-        return true;
-    }
+
 }
